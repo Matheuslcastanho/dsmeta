e2b48e2a8104c4a369af2cca9de704b345de09e4
Matheus Castanho <lucascastanhomatheus@gmail.com>
2022-09-05 16:40:56 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./componentes/notificationButton"
 
 function App() {
- return (<h1>Olá mundo!</h1>)
+ return (
+  <>
+ <h1>Olá mundo!</h1>
+ <NotificationButton/>
+ <NotificationButton/>
+ <NotificationButton/>
+
+ </>
+ )
 }
 
 export default App
