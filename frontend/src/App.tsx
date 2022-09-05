5b443118b0596bb652dcb87a77fc2ec73fbfa2ac
Matheus Castanho <lucascastanhomatheus@gmail.com>
2022-09-05 17:32:59 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import NotificationButton from "./componentes/notificationButton"
+
+import Header from "./componentes/header";
+
+import SalesCard from "./componentes/SalesCard";
 
 function App() {
- return (
-  <>
- <h1>Olá mundo!</h1>
- <NotificationButton/>
- <NotificationButton/>
- <NotificationButton/>
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+        <SalesCard/>
 
- </>
- )
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
